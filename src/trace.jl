be545a46a7b237a4d8f6a7dc96145033fcfbdb17
aviatesk <aviatesk@gmail.com>
2019-07-24 14:03:30 +0900

    Naive migration into Vinyl.jl

--- src/trace.jl
+++ src/trace.jl
@@ -1,6 +1,5 @@
-using Cassette, InteractiveUtils
-
-Cassette.@context TraceurCtx
+using Vinyl: @overdub, @hook
+using InteractiveUtils
 
 struct Trace
   seen::Set
@@ -13,45 +12,41 @@ function Trace(w; maxdepth=typemax(Int))
   Trace(Set(), Vector{Call}(), w, maxdepth)
 end
 
+struct TraceurCtx
+  metadata::Trace
+end
+
 isprimitive(f) = f isa Core.Builtin || f isa Core.IntrinsicFunction
 
 const ignored_methods = Set([@which((1,2)[1])])
 const ignored_functions = Set([getproperty, setproperty!])
 
-function Cassette.prehook(ctx::TraceurCtx, f, args...)
-  tra = ctx.metadata
-  C = DynamicCall(f, args...)
-  push!(tra.stack, C)
-  nothing
-end
+dispatch_type(f, args...) = typeof.((f, args))
 
-function Cassette.posthook(ctx::TraceurCtx, out, f, args...)
-  tra = ctx.metadata
-  C = tra.stack[end]
+should_analyse(tra::Trace, C::DynamicCall) = begin
   f = C.f
-  T = typeof.((f, args))
-  if !(f ∈ ignored_functions || T ∈ tra.seen || isprimitive(f) ||
-       method(C) ∈ ignored_methods || method(C).module ∈ (Core, Core.Compiler))
-    push!(tra.seen, T)
-    analyse((a...) -> tra.warn(Warning(a..., copy(tra.stack))), C)
-  end
-  pop!(tra.stack)
-  nothing
+  T = dispatch_type(f, C.a)
+
+  !(f ∈ ignored_functions || T ∈ tra.seen || isprimitive(f) ||
+    method(C) ∈ ignored_methods || method(C).module ∈ (Core, Core.Compiler))
 end
 
-function Cassette.overdub(ctx::TraceurCtx, f, args...)
+@hook ctx::TraceurCtx (fcall::Any)(fargs...) = begin
   tra = ctx.metadata
-  if (length(tra.stack) > tra.maxdepth)
-    return f(args...)
-  else
-    invoke(Cassette.overdub, Tuple{Cassette.Context, typeof(f), typeof.(args)...}, ctx, f, args...)
+  C = DynamicCall(fcall, fargs...)
+
+  if should_analyse(tra, C)
+    push!(tra.seen, dispatch_type(fcall, fargs))
+    analyse((a...) -> tra.warn(Warning(a...)), C)
   end
 end
 
-trace(w, f; kwargs...) = Cassette.recurse(TraceurCtx(metadata=Trace(w; kwargs...)), f)
+trace(w, fcall, fargs...; kwargs...) = begin
+  @overdub TraceurCtx(Trace(w; kwargs...)) fcall(fargs...)
+end
 
-function warntrace(f; modules=[], kwargs...)
-  trace(warning_printer(modules), f; kwargs...)
+function warntrace(fcall, fargs...; modules=[], kwargs...)
+  trace(warning_printer(modules), fcall, fargs...; kwargs...)
 end
 
 """
@@ -84,7 +79,9 @@ Optional arguments:
 - If `modules` is nonempty, only warnings for methods defined in one of the modules specified will be printed.
 """
 macro trace(ex, args...)
+  fcall = ex.args[1]
+  fargs = ex.args[2:end]
   quote
-    warntrace(() -> $(esc(ex)); $(map(esc, args)...))
+    warntrace($(esc(fcall)), $(map(esc, fargs)...); $(map(esc, args)...))
   end
 end
